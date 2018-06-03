07feda6627b9cfbc9ce9d1dd4a942745e825abc0
vol_nov <2411vova@gmail.com>
2018-06-03 16:44:45 +0300

    Finish work on dormitory part

--- Deploy/init_code_first.bat
+++ Deploy/init_code_first.bat
@@ -1,8 +1,4 @@
 cd ../EDeanery.PL
-
-dotnet ef migrations add AddFacultyIdAndSpecialityId -p ../EDeanery.DAL/EDeanery.DAL.csproj
-
+dotnet ef migrations add RenameDormityRoomName -p ../EDeanery.DAL/EDeanery.DAL.csproj
 dotnet ef database update -p ../EDeanery.DAL/EDeanery.DAL.csproj
-
-
 cd ../Deploy
